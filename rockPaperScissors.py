c87d9bd5d74e6812b71a9c5556a36a1c34a1f721
SUPA <samuelprice@Samuels-MacBook-Air.local>
2025-06-13 12:05:59 -0400

    RockPaperScissors

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -17,7 +17,43 @@ while True:
             break
     if player_choice == 'r':
         print('ROCK versus...')
-    if player_choice == 'r':
-        print('ROCK versus...')
-    if player_choice == 'r':
-        print('ROCK versus...')
+    if player_choice == 'p':
+        print('PAPER versus...')
+    if player_choice == 's':
+        print('SCISSORS versus...')
+    num_choice = random.randint(1, 3)
+    
+    if num_choice == 1:
+        computer_choice = 'r'
+        print('ROCK')
+    elif num_choice == 2:
+        computer_choice = 'p'
+        print('PAPER')
+    elif num_choice == 3:
+        computer_choice = 's'
+        print('SCISSORS')
+    
+    # counter
+    
+    if player_choice == computer_choice:
+        print('Its a tie!')
+        ties = ties + 1
+    elif player_choice == 'r' and computer_choice == 'p':
+        print('You lose, good luck next time!')
+        losses = losses + 1 
+    elif player_choice == 'p' and computer_choice == 's':
+        print('You lose, good luck next time!')
+        losses = losses + 1 
+    elif player_choice == 's' and computer_choice == 'r':
+        print('You lose, good luck next time!')
+        losses = losses + 1 
+    elif player_choice == 'r' and computer_choice == 's':
+        print('You win! Rematch!')
+        wins = wins + 1 
+    elif player_choice == 'p' and computer_choice == 'r':
+        print('You win! Rematch!')
+        wins = wins + 1 
+    elif player_choice == 's' and computer_choice == 'p':
+        print('You win! Rematch!')
+        wins = wins + 1 
+
