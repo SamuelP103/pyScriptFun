83b4ca7897455b8b3f4cf9fcbf189f19b1637e88
SUPA <Samprice0415@gmail.com>
2025-07-01 12:52:24 +0900

    loggingFile

--- FactorialErrorHandling.py
+++ FactorialErrorHandling.py
@@ -1,12 +1,15 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename= 'logfile.txt',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+#Add this to disable the logging after debugging
+logging.disable(logging.CRITICAL)
 logging.debug('Start of program')
-
+# With the logging.debug it will put into the log file due to the 'filename=' above. 
+# It will return only 120 in the end for printing since that is the last in the loop
 def factorial(n):
     logging.debug('Start of factorial(' +str(n)+ ')')
     total = 1
-    for i in range (n + 1):
+    for i in range (1, n + 1):
         total *= i
         logging.debug('i is ' + str(i) +' ' +'total is ' + str(total))
     logging.debug('End of factorial(' + str(n) + ')')
